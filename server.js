a08b39750a0b2257cd75e1b79d8d706020a6536d
Kuba <jakub.mazurek96@gmail.com>
2017-10-31 20:21:25 +0100

    readfilesync instead of require json

--- server.js
+++ server.js
@@ -3,8 +3,12 @@ const express = require('express')
 const net = require('net')
 var app = express();
 
+const read = () => {
+	return JSON.parse(require('fs').readFileSync('./temperature.json', 'utf8'))
+}
+
 app.get("/", (req, res) => {
-	let array = require('fs').readFileSync('./temperature.json')
+	let array = read()
 	res.json(array)
 })
 app.get('/reset', (req, res) => {
@@ -15,7 +19,7 @@ app.get('/reset', (req, res) => {
 	res.send('0')
 })
 app.get("/saveTemperature", (req, res) => {
-	let array = require('fs').readFileSync('./temperature.json')
+	let array = read()
 	if (array.constructor != Array) {
 		array = []
 	}
